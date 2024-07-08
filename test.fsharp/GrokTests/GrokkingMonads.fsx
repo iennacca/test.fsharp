d4006fd8aab71ae0775d5d7bfce3cbb5aee77a77
Jerry Chaves <jerry.chaves@gmail.com>
2024-07-08 16:58:52 -0400

    Corrected list resurn value

--- test.fsharp/GrokTests/GrokkingMonads.fsx
+++ test.fsharp/GrokTests/GrokkingMonads.fsx
@@ -13,13 +13,19 @@ let persons = result {
     let! p3 = Person.Create ("Karen", "Chaves", "09/06/2012", "111-11-4444")
     let! p4 = Person.Create ("Deckard", "Chaves", "09/18/2016", "444-44-4444")
 
-    return [p1, p2, p3, p4]
+    return [p1; p2; p3; p4]
 }
+
+let personlist = 
+    match persons with
+    | Ok l -> l
+    | Error _ -> failwith "Whoops"
+
 
 let personsWithShortCiruitedErrors = result {
     let! p1 = Person.Create ("", "Chaves", "01/01/1999", "")
     let! p2 = Person.Create ("", "Chaves", "09/06/2012", "333-33-3333")
-    return [p1, p2]
+    return [p1; p2]
 }
 
 let personsWithCollatedErrors = result {
