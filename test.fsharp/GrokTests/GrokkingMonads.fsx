52f82db44a62e4ab0475f87f919bc66ff5b8c34c
Jerry Chaves <jerry.chaves@gmail.com>
2024-07-07 20:03:51 -0400

    Renaming tests

--- test.fsharp/GrokTests/GrokkingMonads.fsx
+++ test.fsharp/GrokTests/GrokkingMonads.fsx
@@ -24,7 +24,7 @@ let personsWithShortCiruitedErrors = result {
     return [p1, p2]
 }
 
-let personsWithErrors = result {
+let personsWithCollatedErrors = result {
     let! p1 = Person.Create ("", "Chaves", "01/01/1999", "")
     and! p2 = Person.Create ("", "Chaves", "09/06/2012", "333-33-3333")
     return [p1, p2]
